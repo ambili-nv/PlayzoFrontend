fb862b9db261f752df916f05b0ecd326f7fca35b
ambili-nv <ambilinadvilvalappil@gmail.com>
2024-09-01 23:52:26 +0530

    some changes

--- src/components/user/BannerSlider.tsx
+++ src/components/user/BannerSlider.tsx
@@ -62,7 +62,7 @@ const BannerSlider = () => {
     }, []);
 
     return (
-        <div className="flex justify-center items-center h-screen bg-gradient-to-r from-teal-400 via-green-400 to-slate-400">
+        <div className="flex justify-center items-center h-screen bg-gradient-to-r from-teal-400 via-green-400 to-slate-400 m-10">
             <div className="relative w-full max-w-2xl h-[400px] flex flex-col justify-center items-center text-center bg-white bg-opacity-80 rounded-lg p-6 shadow-lg">
                 <h2 className="text-4xl font-bold text-gray-700 mb-4">FIND PLAYERS & VENUES NEARBY</h2>
                 <p className="text-lg text-gray-600 mb-4">Seamlessly explore sports venues and play with sports enthusiasts just like you!</p>
